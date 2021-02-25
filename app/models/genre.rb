7b8a12e6140d3dd4e9a8275076281b1ec4abc100
hakase-monoshiri <ebenaeleazer@outlook.com>
2021-02-24 21:29:33 -0500

    commit tree 1e53303a131506dcf8ca84befd47bc15c3aec30a

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -3,14 +3,20 @@ class Genre < ActiveRecord::Base
   has_many :artists, through: :songs
 
   def song_count
-    # return the number of songs in a genre
+    self.songs.count
   end
 
   def artist_count
-    # return the number of artists associated with the genre
+    self.artists.count
   end
 
   def all_artist_names
     # return an array of strings containing every musician's name
+    artist_string_array = []
+    Artist.all.map do |artist|
+      artist_string_array << artist.name
+    end
+    artist_string_array
   end
+
 end
